e65a985cb51fb451d3be64d9f0f85d7ecc07a61a
Billy Charlton <billy@okbecause.com>
2024-01-31 16:11:12 +1100

    update computed parquet files with lon/lat for start and end
    add /path endpoint to fetch path elements

--- spark-prepare-data.py
+++ spark-prepare-data.py
@@ -3,10 +3,11 @@
 
 import setuptools
 import pandas as pd
+import math
 from pyspark.conf import SparkConf
 from pyspark.sql import SparkSession, Row
 from pyspark.sql.functions import col, concat, to_json, substring, udf, pandas_udf
-from pyspark.sql.types import StringType
+from pyspark.sql.types import StringType, DoubleType
 
 # Set up Spark with extra ram
 spark = (
@@ -25,10 +26,49 @@ folder = './parquet-data'
 df = spark.read.parquet(f"{folder}/original")
 df.printSchema()
 
-df2 = df.withColumn('start_date', substring("StartTime",1,10))
-# df2 = df2.select(['start_date'])
-df2.printSchema()
+df2 = df
 
+def get_lon(value):
+  coords = value[6:-1].split(' ')
+  return str(round(float(coords[0]), 5))
+
+def get_lat(value):
+  coords = value[6:-1].split(' ')
+  return str(round(float(coords[1]), 5))
+
+def get_trimmed_path(value):
+  trimmed = []
+  points = value.split(',')
+  for point in points:
+      coords = point.split(' ')
+      trim = [str(round(float(p), 5)) for p in coords]
+      trimmed.append(' '.join(trim))
+  return ','.join(trimmed)
+
+lon = udf(get_lon, StringType())
+lat = udf(get_lat, StringType())
+trim_path = udf(get_trimmed_path, StringType())
+
+# start/end dates and times
+df2 = df2.withColumn('start_date', substring("StartTime",1,10))
+df2 = df2.withColumn('start_time', substring("StartTime",12,8))
+df2 = df2.withColumn('end_date', substring("EndTime",1,10))
+df2 = df2.withColumn('end_time', substring("EndTime",12,8))
+
+# start/end points - long/lat
+df2 = df2.withColumn('start_lon', lon(col('first_point')))
+df2 = df2.withColumn('start_lat', lat(col('first_point')))
+df2 = df2.withColumn('end_lon', lon(col('last_point')))
+df2 = df2.withColumn('end_lat', lat(col('last_point')))
+
+# Path1 has far too many digits
+df2 = df2.withColumn('path', trim_path(col('Path1')))
+
+# Drop some columns we'll never use
+drop_cols = ('Path1','Gyro_Roll_path','GyroPitch_path','Gyro_Yaw_path')
+df2.drop(*drop_cols)
+
+df2.printSchema()
 df2.write.parquet(f"{folder}/computed")
 
 # big_df = spark.read.option('mergeSchema','true').parquet(folder)
--- spark-query.py
+++ spark-query.py
@@ -25,6 +25,7 @@ spark = (
 )
 
 # initialize spark dataframe
+# folder = './sample-data/computed'
 folder = './parquet-data/computed'
 
 # df = spark.read.option('mergeSchema','true').parquet(folder)
@@ -37,32 +38,59 @@ df.printSchema()
 
 app = Flask(__name__)
 
+@app.route('/path', methods=['GET'])
+def get_path():
+    # Get query parameters
+    trip = request.args.get('trip')
+    if not trip: raise RuntimeError('need trip')
+    # fetch single trip
+    filtered_df = df.filter(df.TripID==trip)
+    # trimmed = filtered_df.select(['TripID', 'path', 'Timestamp_path'])
+    trimmed = filtered_df.select(['TripID', 'path'])
+    # output
+    json = trimmed.toPandas().to_json(orient='records')
+    return json
+
+
 @app.route('/filter', methods=['GET'])
 def filter_dataframe():
     # Get query parameters
     vehicle = request.args.get('vehicle')
+    veh_type = request.args.get('veh_type')
     trip = request.args.get('trip')
     start_date = request.args.get('start_date')
     end_date = request.args.get('end_time')
-    first_point = request.args.get('first_point')
-    last_point = request.args.get('last_point')
+    start_lon = request.args.get('start_lon')
+    start_lat = request.args.get('start_lat')
+    end_lon = request.args.get('end_lon')
+    end_lat = request.args.get('end_lat')
     distance = request.args.get('distance')
     duration = request.args.get('total_time')
 
     filtered_df = df
 
     # Apply filters
-    if vehicle:
-        # VehicleID = '2fc1f51b3e9f5bc382a7440727e5a954'
-        filtered_df = filtered_df.filter(filtered_df.VehicleID==vehicle)
-    if trip:
-        filtered_df = filtered_df.filter(filtered_df.TripID==trip)
     if start_date:
         filtered_df = filtered_df.filter(filtered_df.start_date==start_date)
     if end_date:
         filtered_df = filtered_df.filter(filtered_df.end_date==end_date)
+    if start_lon:
+        filtered_df = filtered_df.filter(filtered_df.start_lon.startswith(start_lon))
+    if start_lat:
+        filtered_df = filtered_df.filter(filtered_df.start_lat.startswith(start_lat))
+    if end_lon:
+        filtered_df = filtered_df.filter(filtered_df.start_lon.startswith(end_lon))
+    if end_lat:
+        filtered_df = filtered_df.filter(filtered_df.start_lat.startswith(end_lat))
+    if vehicle:
+        filtered_df = filtered_df.filter(filtered_df.VehicleID==vehicle)
+    if veh_type:
+        filtered_df = filtered_df.filter(filtered_df.veh_types==veh_type)
+    if trip:
+        filtered_df = filtered_df.filter(filtered_df.TripID==trip)
 
-    trimmed = filtered_df.select(['start_date','VehicleID','TripID','Path1'])
+    # trimmed = filtered_df
+    trimmed = filtered_df.select(['VehicleID','TripID','start_date','start_time','end_date','end_time','start_lon','start_lat','end_lon','end_lat','total_time','TravelDistanceMeters'])
     # print('COUNT:', trimmed.count())
 
     # data = trimmed.collect()
